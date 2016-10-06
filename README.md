9f8f63cd040e684bb355390fc03b23fdd05daf51
Jan Seidl <seidl@avast.com>
2016-10-06 09:40:02 +0200

    scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Port::Generator - It's new $module
+
+# SYNOPSIS
+
+    use Port::Generator;
+
+# DESCRIPTION
+
+Port::Generator is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/Port/Generator.pm
@@ -0,0 +1,39 @@
+package Port::Generator;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Port::Generator - It's new $module
+
+=head1 SYNOPSIS
+
+    use Port::Generator;
+
+=head1 DESCRIPTION
+
+Port::Generator is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
